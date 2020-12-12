e09db37b12e1c0f65ca16e4b098ca98ae1c7501a
Vladimir <alfa.voland@gmail.com>
2020-12-12 13:52:53 +0300

    Homework_2

---
+++ Geekbrains/2 четверть/JavaScript_basic_course/Lesson_2/Work_5.js
@@ -0,0 +1,19 @@
+/*  Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать
+оператор return. */
+
+function sum(a, b) {
+    return a + b
+}
+
+function difference(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
